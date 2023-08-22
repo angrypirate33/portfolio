dbdd773352a37e02e891e640b8f9269842b51462
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-08-22 13:25:29 -0400

    Adds link to /about to test routing on github pages.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,12 @@
 
+import { Link } from 'react-router-dom'
 import './HomePage.css'
 
 export default function HomePage() {
     return (
-        <h1>Jon's Portfolio Homepage</h1>
+        <div className='HomePage'>
+            <h1>Jon's Portfolio Homepage</h1>
+            <Link to='/about'>About Me</Link>
+        </div>
     )
 }
