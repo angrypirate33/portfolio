e94b8ef12ed6fbded66d38929eae70b0f4ca0c1f
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-08-24 12:20:29 -0400

    Makes small styling tweaks to AboutPage.

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -9,8 +9,10 @@ export default function AboutPage() {
             <div className="wrapper">
                 <h1 className='page-title'>About Me</h1>
                 <AboutMe />
-                <h1 className='logos-title'>Tehcnologies Used</h1>
-                <Logos />
+                <h2 className='logos-title'>Tehcnologies Used</h2>
+                <span className="logos-component">
+                    <Logos />
+                </span>
             </div>
         </div>
     )
