500cab6a57c0a52d68567470dba467b9f53be3ff
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-08-24 08:26:51 -0400

    Adds captions to Logos.

--- src/components/Logos/Logos.jsx
+++ src/components/Logos/Logos.jsx
@@ -17,18 +17,54 @@ export default function Logos() {
     return (
         <div className="Logos">
             <div className="logos-container">
-                <img src={HtmlLogo} alt='HTML Logo' className='logo-item' />
-                <img src={CssLogo} alt='CSS Logo' className='logo-item' />
-                <img src={JavascriptLogo} alt='JavaScript Logo' className='logo-item' />
-                <img src={ReactLogo} alt='React Logo' className='logo-item' />
-                <img src={NodeLogo} alt='Node.js Logo' className='logo-item' />
-                <img src={MongoLogo} alt='MongoDB Logo' className='logo-item' />
-                <img src={PostgresLogo} alt='PostreSQL Logo' className='logo-item' />
-                <img src={SqlLogo} alt='Miscrosoft SQL Logo' className='logo-item' />
-                <img src={MaterializeLogo} alt='Materialize CSS Logo' className='logo-item' />
-                <img src={DjangoLogo} alt='Django Logo' className='logo-item' />
-                <img src={PythonLogo} alt='Python Logo' className='logo-item' />
-                <img src={AwsLogo} alt='Amazon AWS Logo' className='logo-item' />
+                <div className="logo-wrapper">
+                    <img src={HtmlLogo} alt='HTML Logo' className='logo-item' />
+                    <div className="logo-text">HTML</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={CssLogo} alt='CSS Logo' className='logo-item' />
+                    <div className="logo-text">CSS</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={JavascriptLogo} alt='JavaScript Logo' className='logo-item' />
+                    <div className="logo-text">JavaScript</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={ReactLogo} alt='React Logo' className='logo-item' />
+                    <div className="logo-text">React</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={NodeLogo} alt='Node.js Logo' className='logo-item' />
+                    <div className="logo-text">Node</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={MongoLogo} alt='MongoDB Logo' className='logo-item' />
+                    <div className="logo-text">MongoDB</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={PostgresLogo} alt='PostreSQL Logo' className='logo-item' />
+                    <div className="logo-text">PostgreSQL</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={SqlLogo} alt='Miscrosoft SQL Logo' className='logo-item' />
+                    <div className="logo-text">MS SQL Server</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={MaterializeLogo} alt='Materialize CSS Logo' className='logo-item' />
+                    <div className="logo-text">Materialize CSS</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={DjangoLogo} alt='Django Logo' className='logo-item' />
+                    <div className="logo-text">Django</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={PythonLogo} alt='Python Logo' className='logo-item' />
+                    <div className="logo-text">Python</div>
+                </div>
+                <div className="logo-wrapper">
+                    <img src={AwsLogo} alt='Amazon AWS Logo' className='logo-item' />
+                    <div className="logo-text">Amazon AWS</div>
+                </div>
             </div>
         </div>
     )
