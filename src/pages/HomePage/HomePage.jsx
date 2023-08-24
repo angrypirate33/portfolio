ac9eb183219b0a43f0a01927ce8a461691aa2f40
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-08-24 11:41:17 -0400

    Adds/styles linkedin and resume links and images. Adds/styles profile picture. Adds hover effect to Nav links.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,8 @@
 
 import { Link } from 'react-router-dom'
+import ProfilePic from '../../images/me.jpg'
+import LinkedinLogo from '../../images/linkedin.svg'
+import ResumeLogo from '../../images/resume.svg'
 import './HomePage.css'
 
 export default function HomePage() {
@@ -14,6 +17,19 @@ export default function HomePage() {
                         <h3 className="parallax-body">
                             Crafting unique and performant digital solutions.
                         </h3>
+                        <div className="image-wrapper">
+                            <img src={ProfilePic} alt="Jon Schiavone" className="profile-pic" />
+                        </div>
+                        <div className="social-links">
+                            <a href="https://www.linkedin.com/in/jon-schiavone-9436ba21b" target='_blank' rel='noopener noreferrer' className='linkedin-link'>
+                                <img src={LinkedinLogo} alt="Linkedin Logo" className='linkedin-logo' />
+                                LinkedIn
+                            </a>
+                            <a href="/resume.pdf" target='_blank' rel='noopener noreferrer' className='resume-link'>
+                                <img src={ResumeLogo} alt="Resume" className='resume-logo' />
+                                Resume
+                            </a>
+                        </div>
                     </div>
                     <div className="parallax-background"></div>
                 </div>
